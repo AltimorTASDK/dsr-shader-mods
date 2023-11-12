7430f813c9d62236491743f0b82e2cb0e97b3770
AltimorTASDK <altimormc@gmail.com>
2023-11-11 20:42:08 -0500

    fix stack alignment

--- dll/src/hooks/shadows_asm.asm
+++ dll/src/hooks/shadows_asm.asm
@@ -4,10 +4,10 @@ EXTERN create_shadow_map_sampler: PROC
 
 PUBLIC hook_add_shadow_map_sampler
 hook_add_shadow_map_sampler PROC
-        sub     rsp, 20h
+        sub     rsp, 28h
         mov     rcx, r14 ; DLResourceManager
         call    create_shadow_map_sampler
-        add     rsp, 20h
+        add     rsp, 28h
 
         ; Overwritten instructions
         mov     [r14+388h], rsi ; store shadow depth comparison sampler
